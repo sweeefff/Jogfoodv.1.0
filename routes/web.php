492786fda0e7754e0dd0611a3885431aecf135b4
Ridho Putrawan <ridho.putrawan.5@gmail.com>
2025-03-17 19:26:54 +0700

    Praktikum 3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Use App\Http\Controllers\HomeController;
+Use App\Http\Controllers\DataController;
+Use App\Http\Controllers\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/data', [DataController::class, 'data']);
+Route::get('/dashboard', [DashboardController::class, 'dashboard']);
