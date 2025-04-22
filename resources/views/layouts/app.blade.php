f64700b31f2790188de3203e3a88a379fe1e7db0
Ridho Putrawan <ridho.putrawan.5@gmail.com>
2025-04-22 11:31:05 +0700

    penempatan folder view yang benar dan penambahan component card

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>@yield ('title', 'Jogfood app')</title>
+<!-- favicon-->
+<link rel="icon" href="assets/icon/favicon.png" type="image/x-icon">
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!--Bootstrap4 link -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+<link href="assets/css/flowbite.min.css" rel="stylesheet" />
+<script src="assets/css/flowbite.min.js"></script>
