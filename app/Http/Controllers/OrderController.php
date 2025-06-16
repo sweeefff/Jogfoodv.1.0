1eb1bc4d05e7cefb680324596e49aa9728cbbb8f
JosepineStevieHia <steviehia06@gmail.com>
2025-06-16 16:43:22 +0700

    Enhance order management view with transaction details and pagination

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -1,13 +1,18 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Transaksi;
 
 class OrderController extends Controller
 {
     public function order()
     {
-        return view('pages/admin/order');
+        // Ambil semua transaksi beserta user dan detail_transaksi + menu
+        $transaksi = Transaksi::with(['user', 'detail_transaksi.menu'])
+            ->orderByDesc('created_at')
+            ->paginate(10);
+
+        return view('pages.admin.order', compact('transaksi'));
     }
 }
